88feb962a8958f3187604626119cc9b04b85a2b0
CaptB <63907030+CaptBenjamin@users.noreply.github.com>
2022-08-19 19:52:30 +0800

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace 北京天气
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "北京",
+            "昌平",
+            "朝阳",
+            "大兴",
+            "房山",
+            "丰台",
+            "海淀",
+            "怀柔",
+            "门头沟",
+            "密云",
+            "平谷",
+            "石景山",
+            "顺义",
+            "通州",
+            "延庆"});
+            this.comboBox1.Location = new System.Drawing.Point(3, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "刷新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(12, -148);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(776, 1200);
+            this.webBrowser1.TabIndex = 2;
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            this.webBrowser1.NewWindow += new System.ComponentModel.CancelEventHandler(this.webBrowser1_NewWindow);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 32);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "北京天气";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 北京天气
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedItem = "北京";
+            webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/beijing.html");
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh(WebBrowserRefreshOption.Completely);
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "北京")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/beijing.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "昌平")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/changping.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "朝阳")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/chaoyang.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "大兴")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/daxing.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "房山")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/fangshan.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "丰台")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/fengtai.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "海淀")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/haidian.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "怀柔")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/huairou.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "门头沟")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/mentougou.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "密云")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/miyun.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "平谷")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/pinggu.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "石景山")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/shijingshan.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "顺义")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/shunyi.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "通州")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/tongzhou.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+            if (comboBox1.Text == "延庆")
+            {
+                webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/yanqing.html");
+                webBrowser1.ScriptErrorsSuppressed = true;
+            }
+        }
+
+        private void webBrowser1_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            Uri n = webBrowser1.Url;
+            if (comboBox1.Text == "北京")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/beijing.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/beijing.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "昌平")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/changping.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/changping.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "朝阳")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/chaoyang.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/chaoyang.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "大兴")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/daxing.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/daxing.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "房山")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/fangshan.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/fangshan.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "丰台")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/fengtai.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/fengtai.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "海淀")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/haidian.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/haidian.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "怀柔")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/huairou.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/huairou.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "门头沟")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/mentougou.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/mentougou.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "密云")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/miyun.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/miyun.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "平谷")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/pinggu.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/pinggu.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "石景山")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/shijingshan.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/shijingshan.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "顺义")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/shunyi.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/shunyi.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "通州")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/tongzhou.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/tongzhou.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+            if (comboBox1.Text == "延庆")
+            {
+                Uri c = new Uri("http://www.nmc.cn/publish/forecast/ABJ/yanqing.html");
+                if (n != c)
+                {
+                    webBrowser1.Navigate("http://www.nmc.cn/publish/forecast/ABJ/yanqing.html");
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+        }
+
+    }
+}
